995c34d9d3479ce240623729cf126cb1f30e73eb
Malik Elbadri <malik.elbadri@madaproperties.com>
2025-07-25 23:45:19 +0300

    Added get all users to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,44 +1,31 @@
 import BlogCard from "@/components/Cards/BlogCard";
-import UserCard from "@/components/Cards/UserCard";
-import Button from "@/components/Inputs/Button";
 import Container from "@/components/Layout/Container";
-import Section from "@/components/Layout/Section";
 import api from "@/service/api";
 import Link from "next/link";
-import { FaArrowRight } from "react-icons/fa";
 
 export default async function Home() {
-  const { data: blogData } = await api.blogs.getAll({ page: 1, pageSize: 6 });
-  const { data: userData } = await api.users.getAll({ page: 1, pageSize: 6 });
+  const { data: blogData } = await api.blogs.getAll({ page: 1, pageSize: 10 });
   const blogs = blogData?.data;
-  const users = userData?.data;
   return (
     <>
       <Container as="main" className="py-10 flex flex-col mt-[40px] gap-10">
         <h1 className="text-2xl font-bold">Blog Site</h1>
         {blogs && blogs.length ? (
-          <Section
-            link="/blogs"
-            title="Latest Blogs"
-            subTitle="Latest blogs published by the community"
-          >
-            {blogs.map((blog) => (
-              <BlogCard href={`/blogs/${blog.id}`} key={blog.id} blog={blog} />
-            ))}
-          </Section>
+          <section>
+            <h2>Latest Blogs</h2>
+            <div className="grid">
+              {blogs.map((blog) => (
+                <BlogCard
+                  href={`/blogs/${blog.id}`}
+                  key={blog.id}
+                  blog={blog}
+                />
+              ))}
+            </div>
+          </section>
         ) : null}
-
-        {users && users.length ? (
-          <Section
-            link="/users"
-            title="Most Active Users"
-            subTitle="Members with highest participation"
-          >
-            {users.map((user) => (
-              <UserCard href={`/users/${user.id}`} key={user.id} user={user} />
-            ))}
-          </Section>
-        ) : null}
+        <Link href="/blogs">Blogs</Link>
+        <Link href="/users">Users</Link>
       </Container>
     </>
   );
